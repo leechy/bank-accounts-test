dd8f9cec9819edc2fb6056d9de4b28d6533ba0a2
Andrey Lechev <andrey@lechev.me>
2022-11-09 05:32:29 +0100

    Add new Loading element with a spinner

---
+++ test-app/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import { CircularProgress } from "rmwc";
+
+const Loading = () => (
+  <div className="loader--container">
+    <CircularProgress size="large" />
+  </div>
+);
+
+export default Loading;
