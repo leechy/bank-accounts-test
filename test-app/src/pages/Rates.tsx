5433d76cbde3dcb847ae7a5054a57e76b4a865cd
Andrey Lechev <andrey@lechev.me>
2022-11-09 05:30:27 +0100

    Update Rates to use the state as a data source

--- test-app/src/pages/Rates.tsx
+++ test-app/src/pages/Rates.tsx
@@ -1,29 +1,15 @@
+import { useSelector } from "react-redux";
 import { SimpleDataTable, Typography } from "rmwc";
 import AppBar from "../components/AppBar";
 import useMediaQuery from "../hooks/useMediaQuery";
-import { Rates } from "../models/rates";
 
-const rates: Rates = {
-  base: "EUR",
-  date: "2022-11-06",
-  rates: {
-    AUD: 1.543764,
-    CAD: 1.345729,
-    CHF: 0.996466,
-    CNY: 7.173202,
-    DKK: 7.459992,
-    GBP: 0.87748,
-    HKD: 7.837216,
-    JPY: 146.378799,
-    NZD: 1.68541,
-    SEK: 10.989902,
-    SGD: 1.400492,
-    USD: 0.998352,
-    ZAR: 17.87424,
-  },
-};
+import { RootState } from "../store";
 
 const RatesPage = () => {
+  // get the rates from the state
+  const rates = useSelector((state: RootState) => state.rates);
+
+  // how many tables to show depending on the screen size
   const oneColumnTable = useMediaQuery("(max-width: 873px)");
   const twoColumnTable = useMediaQuery("(max-width: 1177px)");
 
@@ -33,6 +19,7 @@ const RatesPage = () => {
         getCellProps={(_, index) => ({ isNumeric: index % 2 === 1 })}
         headers={[["Currency", "Rate for €1"]]}
         data={Object.keys(rates.rates)
+          .sort((a, b) => a[0].localeCompare(b[0]))
           .filter((_, i) => i % table === cols - 1)
           .map((key) => [key, rates.rates[key].toFixed(5)])}
       />
